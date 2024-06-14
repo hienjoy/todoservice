e74490057e050e17408181a60ab2415fe4c9084f
hienjoy <wisdom16no@naver.com>
2024-06-14 22:59:00 +0900

    style: 폰트 적용

--- todo-react-app/src/index.js
+++ todo-react-app/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./AppRouter";
 import reportWebVitals from "./reportWebVitals";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "TodoFont",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRouter />
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
